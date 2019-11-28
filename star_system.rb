11f4ce38883bc7dea460ad075ed2585c70b771b3
Jamie Ryan <56791368+MrJamieRyan@users.noreply.github.com>
2019-11-28 15:30:18 +0000

    stopping point

--- star_system.rb
+++ star_system.rb
@@ -11,11 +11,35 @@ def has_planets(count)
 end
 
 
-def planet_names(planets,)
-  planet_strings = []
-  planets.each {|planets|} << planet_strings
+def planet_names()
+p planets.map {|planets| planets.name}
 end
 
+def get_planet_by_name(planet_name)
+ found_planets = planets.find { |planets| planets.name == planet_name}
+ return found_planets
+end
+
+def get_largest_planet()
+largest_planet = planets.max_by { |planets| planets.diameter}
+return largest_planet
+end
+
+def get_smallest_planet()
+  smallest_planet = planets.min_by { |planets| planets.diameter}
+  return smallest_planet
+end
+
+def get_planets_with_no_moons()
+  planets_with_no_moons = planets.find_all { |planets| planets.number_of_moons == 0 }
+  return planets_with_no_moons
+end
+
+def get_planets_with_more_moons(number)
+get_planets_with_more_moons = planets.find_all { |planets| planets.number_of_moons >= number}
+return get_planets_with_more_moons
+end
+end
 
 
 
