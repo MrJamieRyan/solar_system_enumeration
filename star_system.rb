ec5ff1d3fa66b55dd5254f3cb32155b440eccc75
Jamie Ryan <56791368+MrJamieRyan@users.noreply.github.com>
2019-11-28 14:19:04 +0000

    First commit

---
+++ star_system.rb
@@ -0,0 +1,22 @@
+class StarSystem
+  attr_reader :name, :planets
+
+  def initialize(name, planets)
+      @name = name
+      @planets = planets
+    end
+
+def has_planets(count)
+  return @planets.count
+end
+
+
+def planet_names(planets,)
+  planet_strings = []
+  planets.each {|planets|} << planet_strings
+end
+
+
+
+
+end
